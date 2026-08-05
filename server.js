73d18cc8220903bdf1fee475dfc9f183abb6895c
rcastrogo <rcastrogo@hotmail.com>
2026-08-05 09:02:45 +0200

    fix: api folder

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 // @ts-nocheck
 import express from 'express';
-import usersHandler from './public/api/users.js';
+import usersHandler from './api/users.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static('./public'));
-app.all('/api/users', (req, res) => usersHandler(req, res));
+app.all('/api/users', usersHandler);
 app.listen(PORT, () => {
   console.log(`Servidor Node.js corriendo en http://localhost:${PORT}`);
 });
